725d7c10bcc910a10785f9721289051f3d8606cf
Hui Chen <hchen5796@gmail.com>
2022-11-12 22:12:27 +0100

    Update model_unet.py

--- Development/Training/model_unet.py
+++ Development/Training/model_unet.py
@@ -124,10 +124,7 @@ class up_conv(nn.Module):
     def __init__(self,ch_in,ch_out):
         super(up_conv,self).__init__()
         self.up = nn.Sequential(
-            nn.Upsample(scale_factor=2),
-            nn.Conv2d(ch_in,ch_out,kernel_size=3,stride=1,padding=1,bias=True),
-		        nn.BatchNorm2d(ch_out),
-            nn.ReLU(inplace=True))
+            nn.ConvTranspose2d(ch_in, ch_out, kernel_size=2, stride=2))
 
     def forward(self,x):
         x = self.up(x)
